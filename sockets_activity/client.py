9a062a17e5267c104d2837bcbb3aa58973d6170f
longovinicius <longo.vinicius@gmail.com>
2023-09-26 14:18:03 -0300

    completed client.py

--- sockets_activity/client.py
+++ sockets_activity/client.py
@@ -1,15 +1,62 @@
 import socket
 import time
-from utils import read_cpu_temperature
+import threading
 
+class UDP_Client:
+    def __init__(self) -> None:
+        self.data = []
+        self.ip = None
+        self.port = None
+        self.x = None
+        self.y = None
 
-def send_udp_data():
-    host = '200.135.75.49'
-    port = 54321
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    while True:
-        temperature = read_cpu_temperature()  # Função fictícia para ler a temperatura da CPU
-        s.sendto(temperature.encode('utf-8'), (host, port))
-        time.sleep(1)
+    def SETUP(self, IP, PORTA):
+        self.ip = IP
+        self.port = PORTA
+    
+    def RESET(self):
+        self.data = []
 
-send_udp_data()
+    def SET_X(self, X):
+        self.x = X
+
+    def SET_Y(self, Y):
+        self.y = Y
+
+    def read_cpu_temperature(self):
+        try:
+            with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+                temp = f.read().strip()
+            return float(temp) / 1000.0
+        except FileNotFoundError:
+            return 0.0  # Return a default value
+ 
+    def save_temperature(self):
+        while True:
+            start = time.perf_counter()
+            self.data.append(self.read_cpu_temperature())
+            elapsed = time.perf_counter() - start
+            time.sleep(max(self.y - elapsed, 0))
+
+    def send_average_temperature(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        while True:
+            start = time.perf_counter()
+            if self.data:
+                average = sum(self.data) / len(self.data)
+                s.sendto(str(average).encode('utf-8'), (self.ip, self.port))
+            elapsed = time.perf_counter() - start
+            time.sleep(max(self.x - elapsed, 0))
+
+    def start(self):
+        t1 = threading.Thread(target=self.save_temperature)
+        t2 = threading.Thread(target=self.send_average_temperature)
+        t1.start()
+        t2.start()
+
+# Example usage
+client = UDP_Client()
+client.SETUP("127.0.0.1", 12345)
+client.SET_X(5)
+client.SET_Y(2)
+client.start()
--- sockets_activity/utils.py
+++
@@ -1,8 +0,0 @@
-def read_cpu_temperature():
-    try:
-        with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
-            temp = f.read().strip()
-        return str(int(temp) / 1000.0)
-    except FileNotFoundError:
-        return "Unknown"
-    
